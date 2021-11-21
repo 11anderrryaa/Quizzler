86778a9cfa132afc96e0d1cb0780b7246fd1c6f2
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2021-11-21 08:37:38 -0700

    Configuing Quiz For Multiple Choice

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
     
     var quizBrain = QuizBrain()
@@ -26,19 +27,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
-        print(quizBrain.questionNumber, quizBrain.quiz.count - 1)
-
         let userAnswer = sender.currentTitle!
         let userGotItRight = quizBrain.checkAnswer(userAnswer)
         
         if userGotItRight {
             sender.backgroundColor = .green
-            quizBrain.gotAnswerRight()
+            
         } else {
             sender.backgroundColor = .red
         }
-
+        
         if quizBrain.quiz.count - 1 != quizBrain.questionNumber {
             quizBrain.nextQuestion()
             timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
@@ -49,9 +47,14 @@ class ViewController: UIViewController {
     }
     
     @objc func updateUI() -> Void{
-        trueButton.backgroundColor = .clear
-        falseButton.backgroundColor = .clear
+        Button1.backgroundColor = .clear
+        Button2.backgroundColor = .clear
+        Button3.backgroundColor = .clear
         questionLabel.text = quizBrain.getQuestionText()
+        Button1.titleLabel?.text = quizBrain.quiz[quizBrain.questionNumber].answer[0]
+        Button2.titleLabel?.text = quizBrain.quiz[quizBrain.questionNumber].answer[1]
+        Button3.titleLabel?.text = quizBrain.quiz[quizBrain.questionNumber].answer[2]
+
         progressBar.progress = quizBrain.getProgress()
         scoreLabel.text = "Score: \(quizBrain.score)"
     }
